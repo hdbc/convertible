a07628e32096d423d5d8eb89bb3106b69b3ba264
John Goerzen <jgoerzen@complete.org>
2009-01-23 21:19:03 -0600

    Added Instances.hs

---
+++ Data/Convertible/Instances.hs
@@ -0,0 +1,25 @@
+{-
+Copyright (C) 2009 John Goerzen <jgoerzen@complete.org>
+
+All rights reserved.
+
+For license and copyright information, see the file COPYRIGHT
+
+-}
+
+{- |
+   Module     : Data.Convertible.Instances
+   Copyright  : Copyright (C) 2009 John Goerzen
+   License    : LGPL
+
+   Maintainer : John Goerzen <jgoerzen@complete.org>
+   Stability  : provisional
+   Portability: portable
+
+-}
+
+module Data.Convertible.Instances() where
+
+import Data.Convertible.Instances.Map()
+import Data.Convertible.Instances.Num()
+import Data.Convertible.Instances.Time()
