35bbda9b1402e6a1017b3fe37b130eb91fa1692c
John Goerzen <jgoerzen@complete.org>
2009-01-26 10:12:11 -0600

    Tests are passing.

--- testsrc/TestNum.hs
+++ testsrc/TestNum.hs
@@ -25,8 +25,50 @@ prop_integer_to_int_pass x =
 
 prop_integer_to_word8 :: Integer -> Result
 prop_integer_to_word8 x =
-    safeConvert x @?= Right ((fromIntegral x)::Word8)
+    safeConvert x @?= if x >= fromIntegral (minBound :: Word8) &&
+                         x <= fromIntegral (maxBound :: Word8)
+                      then Right ((fromIntegral x)::Word8)
+                      else Left $ ConvertError (show x) "Integer" "Word8" "Input value outside of bounds: (0,255)"
 
-allt = [q "int -> integer" prop_int_to_integer,
-        q "integer -> int" prop_integer_to_int_pass,
-        q "integer -> word8" prop_integer_to_word8]
+prop_integer_to_word8_safe :: Integer -> Property
+prop_integer_to_word8_safe x =
+    x <= fromIntegral (maxBound :: Word8) &&
+    x >= fromIntegral (minBound :: Word8) ==>
+      safeConvert x @?= Right ((fromIntegral x)::Word8)
+
+prop_integer_to_word8_unsafe :: Integer -> Property
+prop_integer_to_word8_unsafe x =
+    x < fromIntegral (minBound :: Word8) ||
+    x > fromIntegral (maxBound :: Word8) ==>
+      ((safeConvert x)::ConvertResult Word8) @?= (Left $ ConvertError (show x) "Integer" "Word8" "Input value outside of bounds: (0,255)")
+
+prop_double_to_word8 :: Double -> Result
+prop_double_to_word8 x =
+    safeConvert x @?= if truncate x >= toInteger (minBound :: Word8) &&
+                         truncate x <= toInteger (maxBound :: Word8)
+                      then Right ((truncate x)::Word8)
+                      else Left $ ConvertError (show x) "Double" "Word8" "Input value outside of bounds: (0,255)"
+
+prop_double_to_word8_safe :: Double -> Property
+prop_double_to_word8_safe x =
+    x <= fromIntegral (maxBound :: Word8) &&
+    x >= fromIntegral (minBound :: Word8) ==>
+      safeConvert x @?= Right ((truncate x)::Word8)
+
+prop_double_to_word8_unsafe :: Double -> Property
+prop_double_to_word8_unsafe x =
+    truncate x < toInteger (minBound :: Word8) ||
+    truncate x > toInteger (maxBound :: Word8) ==>
+      ((safeConvert x)::ConvertResult Word8) @?= (Left $ ConvertError (show x) "Double" "Word8" "Input value outside of bounds: (0,255)")
+
+allt = [q "Int -> Integer" prop_int_to_integer,
+        q "Integer -> Int (safe bounds)" prop_integer_to_int_pass,
+        q "Integer -> Word8 (general)" prop_integer_to_word8,
+        q "Integer -> Word8 (safe bounds)" prop_integer_to_word8_safe,
+        q "Integer -> Word8 (unsafe bounds)" prop_integer_to_word8_unsafe,
+        q "Double -> Word8 (general)" prop_double_to_word8,
+        q "Double -> Word8 (safe bounds)" prop_double_to_word8_safe,
+        q "Double -> Word8 (unsafe bounds)" prop_double_to_word8_unsafe
+       ]
+
+
