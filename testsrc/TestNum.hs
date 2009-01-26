69accd53e1072308f7cc2081756b360d6ef89628
John Goerzen <jgoerzen@complete.org>
2009-01-26 09:48:21 -0600

    Working on tests

--- testsrc/TestNum.hs
+++ testsrc/TestNum.hs
@@ -11,9 +11,22 @@ import TestInfrastructure
 import Data.Convertible
 import Test.QuickCheck
 import Test.QuickCheck.Tools
+import Data.Word
 
 prop_int_to_integer :: Int -> Result
 prop_int_to_integer x =
     safeConvert x @?= Right ((fromIntegral x)::Integer)
 
-allt = [q "int -> integer" prop_int_to_integer]
+prop_integer_to_int_pass :: Integer -> Property
+prop_integer_to_int_pass x =
+    (x <= fromIntegral (maxBound :: Int)) &&
+    (x >= fromIntegral (minBound :: Int)) ==> 
+                                          safeConvert x @?= Right ((fromIntegral x)::Int)
+
+prop_integer_to_word8 :: Integer -> Result
+prop_integer_to_word8 x =
+    safeConvert x @?= Right ((fromIntegral x)::Word8)
+
+allt = [q "int -> integer" prop_int_to_integer,
+        q "integer -> int" prop_integer_to_int_pass,
+        q "integer -> word8" prop_integer_to_word8]
