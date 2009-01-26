7ccdfda4aa00dbb6402071c66fba896bc294007e
John Goerzen <jgoerzen@complete.org>
2009-01-26 10:46:22 -0600

    Added TestMap.hs

--- testsrc/runtests.hs
+++ testsrc/runtests.hs
@@ -3,11 +3,13 @@ import qualified Test.HUnit as HU
 import Test.HUnit.Tools
 
 import qualified TestNum
+import qualified TestMap
 
 test1 = HU.TestCase ((HU.@=?) "x" "x")
 
 alltests = [HU.TestLabel "test1" test1,
-            tl "TestNum" TestNum.allt]
+            tl "TestNum" TestNum.allt,
+            tl "TestMap" TestMap.allt]
 
 main = do runVerboseTests (HU.TestList alltests)
           return ()
