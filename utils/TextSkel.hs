38f8b9195f34788c456e7a2b177a119021944ceb
Michael Snoyman <michael@snoyman.com>
2009-12-07 21:34:08 +0200

    Removed redundant ConvertAttempt instances from Text

--- utils/TextSkel.hs
+++ utils/TextSkel.hs
@@ -7,7 +7,5 @@ pairs = do
 
 main = do
     mapM_ cs pairs
-    mapM_ ca pairs
 
 cs (f, t) = putStrLn $ "instance ConvertSuccess " ++ f ++ " " ++ t ++ " where\n    convertSuccess = "
-ca (f, t) = putStrLn $ "instance ConvertAttempt " ++ f ++ " " ++ t ++ " where\n    convertAttempt = return . convertSuccess"
