ec483c0c8772bac20556634f10117e79d2893364
Michael Snoyman <michael@snoyman.com>
2009-12-07 21:30:16 +0200

    Removed redundant ConvertAttempt instances from Num

--- utils/genNumInstances.hs
+++ utils/genNumInstances.hs
@@ -20,8 +20,6 @@ showIntToInteger :: String -> String
 showIntToInteger i =
     "instance ConvertAttempt Integer " ++ i ++ " where\n\
     \    convertAttempt = boundedConversion (return . fromIntegral)\n\
-    \instance ConvertAttempt " ++ i ++ " Integer where\n\
-    \    convertAttempt = return . fromIntegral\n\
     \instance ConvertSuccess " ++ i ++ " Integer where\n\
     \    convertSuccess = fromIntegral\n"
 
@@ -36,9 +34,7 @@ showFloatToInt (f, i) =
     "instance ConvertAttempt " ++ f ++ " " ++ i ++ " where\n\
     \    convertAttempt = boundedConversion (return . truncate)\n\
     \instance ConvertSuccess " ++ i ++ " " ++ f ++ " where\n\
-    \    convertSuccess = fromIntegral\n\
-    \instance ConvertAttempt " ++ i ++ " " ++ f ++ " where\n\
-    \    convertAttempt = return . fromIntegral\n"
+    \    convertSuccess = fromIntegral\n"
 
 floatToFloatWork :: [(String, String)]
 floatToFloatWork = do
@@ -50,9 +46,7 @@ floatToFloatWork = do
 showFloatToFloat :: (String, String) -> String
 showFloatToFloat (src, dst) =
     "instance ConvertSuccess " ++ src ++ " " ++ dst ++ " where\n\
-    \    convertSuccess = " ++ convertFunc ++ "\n\
-    \instance ConvertAttempt " ++ src ++ " " ++ dst ++ " where\n\
-    \    convertAttempt = return . " ++ convertFunc ++ "\n"
+    \    convertSuccess = " ++ convertFunc ++ "\n"
     where
         convertFunc
             | dst == "Rational" = "toRational"
@@ -61,12 +55,8 @@ showFloatToFloat (src, dst) =
 showFloatToInteger f =
     "instance ConvertSuccess Integer " ++ f ++ " where\n\
     \    convertSuccess = fromIntegral\n\
-    \instance ConvertAttempt Integer " ++ f ++ " where\n\
-    \    convertAttempt = return . fromIntegral\n\
     \instance ConvertSuccess " ++ f ++ " Integer where\n\
-    \    convertSuccess = truncate\n\
-    \instance ConvertAttempt " ++ f ++ " Integer where\n\
-    \    convertAttempt = return . truncate\n"
+    \    convertSuccess = truncate\n"
 
 showIntToChar i =
     "instance ConvertAttempt Char " ++ i ++ " where\n\
